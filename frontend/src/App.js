301a3fb5a19821ec34623f9a4da65345ab087a27
Piyush Motwani <piyushmotwani211@gmail.com>
2023-07-07 14:29:25 +0530

    Login/Out SignUp Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,18 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
-            <Route 
-
-            path='/'
-            element={<Home/>}
-            />
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
